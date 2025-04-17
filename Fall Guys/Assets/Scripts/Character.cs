927681c82d58fdc545c7a6f62cfb405d5304ecbc
kitsunera1138 <kitsunera1138@gmail.com>
2025-04-17 15:43:49 +0900

    (2025.4.17) 동기화 시스템

--- Fall Guys/Assets/Scripts/Character.cs
+++ Fall Guys/Assets/Scripts/Character.cs
@@ -3,14 +3,61 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun; //MonoBehaviourPun
 
+[RequireComponent(typeof(Rotation))]
 public class Character : MonoBehaviourPun
 {
+    [SerializeField] float speed;
+    [SerializeField] Vector3 direction;
+    [SerializeField] Rotation rotation;
     [SerializeField] Camera virtualCamera;
+    [SerializeField] CharacterController characterController;
 
+    private void Awake()
+    {
+        rotation = GetComponent<Rotation>();
+        characterController = GetComponent<CharacterController>();
+        if (speed == 0) speed = 5f;
+    }
     void Start()
     {
-        if(virtualCamera == null) virtualCamera = GetComponentInChildren<Camera>();
+        if (virtualCamera == null) virtualCamera = GetComponentInChildren<Camera>();
         DisableCamera();
+    }
+
+    private void Update()
+    {
+        //나 자신이 아니면 움직이지 못하도록함
+        if (photonView.IsMine == false) return;
+
+
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.z = Input.GetAxisRaw("Vertical");
+
+        direction.Normalize();
+
+        Move();
+
+        Rotate();
+    }
+
+    void Rotate()
+    {
+        rotation.OnMouseY();
+
+        rotation.RotateY(transform);
+    }
+
+    public void Move()
+    {
+        //direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical") ).normalized;
+
+        //if (direction != Vector3.zero)
+        //characterController.Move(direction * speed * Time.deltaTime);
+
+        //바라보는 좌표로 이동하는 방식으로 변경
+        Vector3 modifiedTransform = transform.TransformDirection(direction * speed * Time.deltaTime);
+        characterController.Move(modifiedTransform);
+
     }
 
     void DisableCamera()
--- Fall Guys/Assets/Scripts/CharacterManager.cs
+++ Fall Guys/Assets/Scripts/CharacterManager.cs
@@ -3,27 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 
-
 public class CharacterManager : MonoBehaviour
 {
-    [SerializeField] Transform spawnPostion;
-    [SerializeField] List<Vector3> spawnPos;
-    float maxCount;
+    [SerializeField] List<Transform> transformList = new List<Transform>();
+
+    private void Awake()
+    {
+        CreateTransform();
+    }
     void Start()
     {
-        //현재 방의 maxPlayer
-        maxCount = PhotonNetwork.CurrentRoom.MaxPlayers;
-        for(int i =0; i< maxCount; i++)
-        {
-            spawnPos.Add(new Vector3(i+1,1.3f,0));
-        }
-
         //Debug.Log(PhotonNetwork.LocalPlayer.ActorNumber); //get; //나갔다 들어오면 중첩이 가능해서 
-        //Debug.Log(PhotonNetwork.CurrentRoom.PlayerCount); //CurrentRoom.PlayerCount 사용
+        //Debug.Log(PhotonNetwork.CurrentRoom.PlayerCount); //CurrentRoom.PlayerCount 사용 //1부터 시작됨
 
-        //PhotonNetwork.Instantiate 방에 들어와야 생성됨 - 로그인해서 등등
-        GameObject clone = PhotonNetwork.Instantiate("Character", Vector3.zero, Quaternion.identity);
-        clone.transform.position = spawnPos[PhotonNetwork.CurrentRoom.PlayerCount];
+        int index = PhotonNetwork.CurrentRoom.PlayerCount - 1; //1부터 시작해서 -1
+
+        //PhotonNetwork.Instantiate 방에 들어와야 생성됨 - 로그인해서 등등 
+        //PhotonNetwork.Instantiate ( string Resources 폴더 안 오브젝트 이름,
+        //Quaternion.identity 회전 X (회전값 없음)
+        GameObject clone = PhotonNetwork.Instantiate("Character", transformList[index].position, Quaternion.identity);
     }
 
+    void CreateTransform()
+    {
+        for (int i = 0; i < PhotonNetwork.CurrentRoom.MaxPlayers; i++)
+        {
+            Transform clone = Instantiate(Resources.Load<Transform>("Create Position " + i));
+            transformList.Add(clone);
+        }
+    }
 }
